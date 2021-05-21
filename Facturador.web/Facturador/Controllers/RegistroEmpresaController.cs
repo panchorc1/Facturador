4075353c4a02e37f7e57743967814d53375156da
Francisco Rodríguez <panchorc1@gmail.com>
2021-05-20 20:02:46 -0600

    Carga de Proyecto inicial

---
+++ Facturador.web/Facturador/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Facturador.Controllers
+{
+    public class RegistroEmpresaController : Controller
+    {
+        public ActionResult RegistroEmpresa()
+        {
+            return View();
+        }
+
+    }
+}
